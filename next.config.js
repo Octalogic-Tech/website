1b4815a839157e76cb767e01deb70b4ae9fb513f
Jude Fernandes <jude.manutd@gmail.com>
2022-11-13 15:16:03 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // experimental: {
+  //   runtime: "experimental-edge",
+  // },
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+};
+
+module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+import HorizontalRule from "../components/horizontal-rule/horizontal-rule";
+
+const Home = () => {
+  return (
+    <>
+      <Box sx={{ minHeight: "100vh" }}>
+        <h3>Home page</h3>
+      </Box>
+      <HorizontalRule />
+    </>
+  );
+};
+
+export default Home;
