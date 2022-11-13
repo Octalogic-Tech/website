7e69b5fc6af3c85002a3cd490fc8c80dd5539b9b
Jude Fernandes <judemanutd@users.noreply.github.com>
2022-11-13 20:26:24 +0530

    SEO Support (#2)
    * added seo support
    * build fixes
    * logo fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,28 +2,19 @@ import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 
-import theme, { comfortaa } from "../utils/theme";
-import createEmotionCache from "../utils/createEmotionCache";
+import theme, { comfortaa } from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className={comfortaa.className}>
-        {/* <Html lang="en"> */}
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <meta name="msapplication-TileColor" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
-
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap"
-            rel="stylesheet"
-          /> */}
         </Head>
         <body>
           <Main />
