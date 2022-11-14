26c7ee88644e103ab59cb965d7bccaa26c7d2555
dylanofoctalogic <dylan.t@octalogic.in>
2022-11-14 12:57:24 +0530

    Fixed lint issue.

--- components/service-card/service-card.tsx
+++ components/service-card/service-card.tsx
@@ -6,7 +6,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
-import Box from "@mui/system/Box";
+// import Box from "@mui/system/Box";
 
 import MobileDevCard from "../../public/images/mobile_dev_card.svg";
 
