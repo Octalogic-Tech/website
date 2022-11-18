d8ed0457db629e24da55bbc7b1d7c064adddb969
dylanofoctalogic <dylan.t@octalogic.in>
2022-11-18 11:18:45 +0530

    Web development page complete.

--- components/tech-card/tech-card.tsx
+++ components/tech-card/tech-card.tsx
@@ -7,9 +7,36 @@ import { ITechnology } from "../../interfaces";
 export const TechCard = ({ details }: { details: ITechnology }) => {
   const { name, iconUrl, link } = details;
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", flex: "0 0 10%", padding: "1rem" }}>
-      <Box sx={{ position: "relative", width: "100%", height: "100%", textAlign: "center" }}>
-        <Image src={iconUrl} alt={`${name} logo`} width={112} height={112} />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flex: "0 0 10%",
+        padding: "1rem",
+        transition: "box-shadow .3s, border-radius .3s",
+        ":hover": {
+          borderRadius: ".25rem",
+          boxShadow: "0.062rem 1.25rem 1.562rem -0.312rem rgb(46 61 73 / 30%)",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src={iconUrl}
+          alt={`${name} logo`}
+          width={112}
+          height={0}
+          style={{ height: "auto" }}
+        />
       </Box>
       <a
         style={{
