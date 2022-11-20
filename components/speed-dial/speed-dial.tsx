db1316236a4a9569b7913b2b64b7a48b8f4e121d
Glenn Fernandes <glennferns93@gmail.com>
2022-11-20 21:07:09 +0530

    linting fixes

--- components/speed-dial/speed-dial.tsx
+++ components/speed-dial/speed-dial.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
 
 import HomeIcon from "@mui/icons-material/Home";
@@ -10,6 +11,7 @@ import GroupsIcon from "@mui/icons-material/Groups";
 import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
 import PermPhoneMsgIcon from "@mui/icons-material/PermPhoneMsg";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 import { useRouter } from "next/router";
 
@@ -47,16 +49,19 @@ export function MobileSpeedDial() {
       <SpeedDial
         ariaLabel="SpeedDial"
         // sx={{ position: 'absolute', bottom: 0, right: 16 }}
-        icon={<MenuIcon />}
+        icon={<SpeedDialIcon icon={<MenuIcon />} openIcon={<CloseIcon />} />}
         onClick={handleClick}
         open={open}
-        direction={"left"}
+        direction={"up"}
       >
         {actions.map((action) => (
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
             onClick={() => handleActionClick(action.linkUrl)}
+            sx={{ backgroundColor: "primary.main", color: "white" }}
+            // tooltipTitle={action.name}
+            // tooltipOpen
           />
         ))}
       </SpeedDial>
