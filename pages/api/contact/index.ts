7421c1367dd0fdde32cee835b27698da3c502744
Jude Fernandes <jude.manutd@gmail.com>
2022-11-21 22:57:55 +0530

    env logs

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { IFormData } from "../../../interfaces";
 
-import { slackWebhookUrl } from "../../../config/vars";
+import * as vars from "../../../config/vars";
 
 const formatData = (data: IFormData) => {
   const formattedData = Object.keys(data).map((key) => {
@@ -32,6 +32,9 @@ const formatData = (data: IFormData) => {
 };
 
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+  // eslint-disable-next-line no-console
+  console.log("🚀 ~ file: index.ts ~ line 6 ~ vars", vars);
+
   if (req.method === "POST") {
     const formData = req.body;
     const slackData = formatData(formData);
@@ -41,9 +44,9 @@ export default async function Handler(req: NextApiRequest, res: NextApiResponse)
       body: JSON.stringify(slackData),
     };
 
-    if (slackWebhookUrl)
+    if (vars.slackWebhookUrl)
       try {
-        await fetch(slackWebhookUrl, options);
+        await fetch(vars.slackWebhookUrl, options);
         res.status(201).json({ message: "Success" });
       } catch (error) {
         res.status(500).json({ message: "Unable to save data" });
