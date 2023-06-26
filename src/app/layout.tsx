c3d9e5d6cc33b3e1992fa02693604867ec6e3f01
Jude Fernandes <jude.manutd@gmail.com>
2023-06-26 18:34:12 +0530

    audit fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,7 +84,7 @@ const jsonLd = {
       ],
       contactPoint: [
         {
-          telephone: "+919096302367",
+          telephone: "+919209301230",
           contactType: "technical and finance support",
           email: "carlton@octalogic.in",
           areaServed: ["Dubai", "India"],
@@ -105,7 +105,7 @@ const jsonLd = {
           availableLanguage: ["English", "Hindi", "Konkani"],
         },
         {
-          telephone: "+919923358164",
+          telephone: "+918830669189",
           contactType: "technical support",
           email: "jude@octalogic.in",
           areaServed: ["US", "Dubai", "Asia"],
