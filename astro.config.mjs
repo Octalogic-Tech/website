ddd0ad14928172f29f2fa7018c886e6caf8584ab
yogioctalogic <shreyog@octalogic.in>
2025-07-02 12:23:17 +0530

    refactor: Remove Tailwind CSS configuration and update styles - Deleted postcss.config.js and tailwind.config.js files. - Updated globals.css to import Tailwind CSS directly. - Added a sample heading in index.astro to demonstrate Tailwind CSS usage. - Cleaned up unused Tailwind CSS layers in globals.css.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  // ...
-  integrations: [// ...
-  react(), 
-  tailwind({
-      applyBaseStyles: false,
-    }),],
+  integrations: [
+    react(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
